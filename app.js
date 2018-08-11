2a4e6c816fe5f29cd0efd4c69784986254981ddd
djjelly <omardiaz2012@gmail.com>
2018-08-11 01:17:30 -0700

    Server Changes

---
+++ app.js
@@ -0,0 +1,13 @@
+
+////* This is where the other pages are connected to *////
+
+const express = require('express');
+const app = express();
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes); 
+    
+module.exports = app;
